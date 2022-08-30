7f5a6749a00a65e1dbee1df40e938e6d54d5aac6
Hans97a <byby8992@naver.com>
2022-08-30 23:27:56 +0900

    deploy

--- blog/settings.py
+++ blog/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['django-env.eba-u2yrkhme.ap-northeast-2.elasticbeanstalk.com']
 
 
 # Application definition
