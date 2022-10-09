a5f97960581b49e0ab2bbba2c99794ebd6df4849
Hans97a <byby8992@naver.com>
2022-10-09 20:40:01 +0900

    storage css superuser

--- blog/custom_storages.py
+++ blog/custom_storages.py
@@ -1,8 +1,8 @@
-from storages.backends.s3boto3 import S3Boto3Storages
+from storages.backends.s3boto3 import S3Boto3Storage
 
-class StaticStorage(S3Boto3Storages):
+class StaticStorage(S3Boto3Storage):
     location = "static/"
     file_overwrite = False
 
-class UploadStorage(S3Boto3Storages):
+class UploadStorage(S3Boto3Storage):
     location = "uploads/"
